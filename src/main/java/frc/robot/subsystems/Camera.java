cb8ece8c6606fee9dfdde88c0511b26ec3f27712
Kbratland <116665821+Kbratland@users.noreply.github.com>
2022-11-09 09:36:45 -0800

    e

--- src/main/java/frc/robot/subsystems/Camera.java
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -7,12 +7,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Camera extends SubsystemBase{
     PhotonCamera photonCamera = new PhotonCamera(null, getName());
-<<<<<<< Updated upstream
    public double getTargetYaw(){
-=======
-   public double getTargetYaw()
-   {
->>>>>>> Stashed changes
        var result = photonCamera.getLatestResult();
        PhotonTrackedTarget target = result.getBestTarget();
        return target.getYaw();
