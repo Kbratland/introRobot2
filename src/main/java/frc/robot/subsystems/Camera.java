b95056d2252491e0ea64f4e9067d450379962612
Kbratland <116665821+Kbratland@users.noreply.github.com>
2022-11-09 09:48:58 -0800

    e

--- src/main/java/frc/robot/subsystems/Camera.java
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -6,7 +6,7 @@ import org.photonvision.targeting.PhotonTrackedTarget;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Camera extends SubsystemBase{
-    PhotonCamera photonCamera = new PhotonCamera(null, getName());
+    PhotonCamera photonCamera = new PhotonCamera("MICROSOFT_LifeCam_HD-3000");
    public double getTargetYaw(){
        var result = photonCamera.getLatestResult();
        PhotonTrackedTarget target = result.getBestTarget();
