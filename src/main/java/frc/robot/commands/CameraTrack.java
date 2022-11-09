78806e7bd7117abbab563d97d6c7b12bd7e9d542
Kbratland <116665821+Kbratland@users.noreply.github.com>
2022-11-09 09:22:50 -0800

    e

--- src/main/java/frc/robot/commands/CameraTrack.java
+++ src/main/java/frc/robot/commands/CameraTrack.java
@@ -1,14 +1,36 @@
 package frc.robot.commands;
 
 import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveSubsystem;
 
 public class CameraTrack extends CommandBase {
     PhotonCamera photonCamera = new PhotonCamera(null, getName());
+    var result = photonCamera.getLatestResult();
+    DriveSubsystem driveSubsystem;
+    PhotonTrackedTarget target = result.getBestTarget();
+    double yaw = target.getYaw();
+    public void getCamera(PhotonCamera photonCamera, DriveSubsystem driveSubsystem) {
+      this.photonCamera = photonCamera;
 
-    public void getCamera() {
-      var result = photonCamera.getLatestResult();
-      boolean hasTargets = result.hasTargets();
+      // boolean hasTargets = result.hasTargets();
       
+      // double pitch = target.getPitch();
+      // double area = target.getArea();
+      // double skew = target.getSkew();
+      // ransform2d pose = target.getCameraToTarget();
+      // List<TargetCorner> corners = target.getCorners();
     }
+      @Override
+      public void initialize(){
+
+      }
+      public void execute(){
+        double yaw = target.getYaw();
+        if(yaw < 0 ){
+          driveSubsystem.arcadeDrive(0, 0.5);
+        }
+      }
 }
