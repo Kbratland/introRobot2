d07e02282ed2d99efbee18c0dc362256ff773bd9
Kbratland <116665821+Kbratland@users.noreply.github.com>
2022-11-14 09:49:16 -0800

    ytrhtrhrthrth

--- src/main/java/frc/robot/subsystems/Camera.java
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -8,10 +8,10 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Camera extends SubsystemBase{
     PhotonCamera photonCamera = new PhotonCamera("Microsoft_LifeCam_HD-3000");
    public double getTargetYaw(){
-       var result = photonCamera.getLatestResult();
-       System.out.println("Result is " + result.toString());
-       if(result.hasTargets()){
-        PhotonTrackedTarget target = result.getBestTarget();
+    //    var result = photonCamera.getLatestResult();
+    //    System.out.println("Result is " + result.toString());
+       if(photonCamera.getLatestResult().hasTargets()){
+        PhotonTrackedTarget target = photonCamera.getLatestResult().getBestTarget();
         System.out.println("I have a target " + target);
         return target.getYaw();
        }
