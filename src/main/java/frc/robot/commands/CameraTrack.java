660767a2acddef6ab17ea28f3c36e8bf8dc9fc84
Flamepupper <113953766+Flamepupper@users.noreply.github.com>
2022-11-15 10:11:28 -0800

    bad make dents in walls machine

--- src/main/java/frc/robot/commands/CameraTrack.java
+++ src/main/java/frc/robot/commands/CameraTrack.java
@@ -15,28 +15,56 @@ public class CameraTrack extends CommandBase {
 
     }
     public void execute(){
+      if (camera.getLatestResult().hasTargets()) {
         double a = camera.getTargetArea();
         double x = camera.getTargetYaw();
         System.out.println("depth " + a + ", Yaw " + x);
         //driveSubsystem.arcadeDrive(0, yaw * -1);
-        if(x < 0){
-          driveSubsystem.arcadeDrive(0, -0.25);
-        }
-        else if (x > -0.3 && x < 0.3){
+        if (x > -0.3 && x < 0.3){
           driveSubsystem.arcadeDrive(0, 0);
         }
-        else if (x > 0){
-          driveSubsystem.arcadeDrive(0, 0.25);
+        if(x < -0.3){
+          double r = 0;
+          if (x > -10 && x > 5 ){
+            r = (x/10)*-1;
+          }
+          else {
+            r = -0.5;
+          }
+          driveSubsystem.arcadeDrive(0, r);
         }
-        if(a < 5){
-          driveSubsystem.arcadeDrive(0.25, 0);
+        else if(x < 10 && x > 5){
+          double r = 0;
+          if (x < 10 && x > 5){
+          r = (x/10); 
+          }
+          else {
+            r = 0.5;
+          }
+          driveSubsystem.arcadeDrive(0, r);
+        }
+        if(a < 5){ 
+          //0=1 & 5 = 0.5
+          // 5 - 0, 0.5 - 1
+          // -5 to 0 +5
+          //a = (a+5)/10;
+          a = (a * -1) + 5;
+          a = a/5;
+          if (a < 0.5){
+          a = 0.5;
+          }
+          driveSubsystem.arcadeDrive(a , 0);
         }
         else if (a > 20 && x < 5){
           driveSubsystem.arcadeDrive(0, 0);
         }
         else if (a > 20){
-          driveSubsystem.arcadeDrive(-0.25, 0);
+          a = a/40;
+          // 30 - 20, 1 - 0.5
+
+          driveSubsystem.arcadeDrive(-a, 0);
         }
+      }
         // else if(yaw == null){
         // driveSubsystem.arcadeDrive(0, 0);
         // }
